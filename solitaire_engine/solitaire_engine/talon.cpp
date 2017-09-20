c466cdb91a5cec120b7f8aa6052f5db6560eaa72
arthurquinn92 <arthurquinn92@gmail.com>
2017-09-19 20:44:48 -0400

    implement talon push

--- solitaire_engine/solitaire_engine/talon.cpp
+++ solitaire_engine/solitaire_engine/talon.cpp
@@ -2,11 +2,13 @@
 #include "talon.h"
 
 
-Talon::Talon()
-{
+Talon::Talon() {
 }
 
+void Talon::push(Card* card) {
+	card->flip(true);
+	pile.push_back(card);
+}
 
-Talon::~Talon()
-{
+Talon::~Talon() {
 }
--- solitaire_engine/solitaire_engine/talon.h
+++ solitaire_engine/solitaire_engine/talon.h
@@ -1,8 +1,14 @@
 #pragma once
-class Talon
+#include "card_pile.h"
+
+class Talon : public CardPile
 {
 public:
 	Talon();
+
+	// push a card onto the talon and flip it face up
+	void push(Card* card) override;
+
 	~Talon();
 };
 
