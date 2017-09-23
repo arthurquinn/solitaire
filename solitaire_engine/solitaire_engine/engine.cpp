acd43214aa40323cef7acce49580982f0dfbff4b
arthurquinn92 <arthurquinn92@gmail.com>
2017-09-23 16:54:51 -0400

    nice logic mo

--- solitaire_engine/solitaire_engine/engine.cpp
+++ solitaire_engine/solitaire_engine/engine.cpp
@@ -10,13 +10,7 @@ void Engine::init() {
   for (int i = 0; i < NUM_CARDS; i++) {
     rank_t rank = i % NUM_CARDS_IN_SUIT;
     suit_t suit = i / NUM_CARDS_IN_SUIT;
-    color_t color = RED;
-
-    if ((i >= 13 && i <= 25) || (i >= 39 && i <= 51)) {
-      color = BLACK;
-    }
-
-    stock.push(new Card(suit, rank, color));
+    stock.push(new Card(suit, rank, suit == CLUBS || suit == SPADES));
   }
   stock.shuffle();
 }
