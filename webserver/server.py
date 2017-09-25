659fdc3319a504073f9a1ed282a53730e1f8faf8
arthurquinn <arthurquinn92@gmail.com>
2017-09-24 22:09:32 -0400

    rendered a static file with flask

--- webserver/server.py
+++ webserver/server.py
@@ -3,4 +3,12 @@
 from flask import Flask, render_template
 from flask_socketio import SocketIO
 
-print("Hello")
+app = Flask(__name__)
+socketio = SocketIO(app)
+
+@app.route("/")
+def index():
+  return render_template("index.html")
+
+if __name__ == "__main__":
+  app.run(port=8080)
